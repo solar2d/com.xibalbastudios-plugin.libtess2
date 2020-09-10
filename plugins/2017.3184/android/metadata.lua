9411b542dbf16fb3a4786a8109e342cc0d0ec06f
Vlad Shcherban <vlad@coronalabs.com>
2020-09-10 14:54:31 +0300

    Updating binaries

---
+++ plugins/2017.3184/android/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'sharedLibrary',
+		staticLibs = { 'plugin.libtess2', },
+		frameworks = {},
+		frameworksOptional = {},
+	},
+}
+
+return metadata
---
+++ plugins/2017.3184/iphone-sim/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'plugin_libtess2', },
+		frameworks = {},
+		frameworksOptional = {},
+	},
+}
+
+return metadata
